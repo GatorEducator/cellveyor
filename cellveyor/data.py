203020fc751779b5991fffd8d7e48b690005c6f5
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-10-09 16:41:09 -0400

    coms: Improve the docstring comment in data.py.

--- cellveyor/data.py
+++ cellveyor/data.py
@@ -7,6 +7,6 @@ import pandas
 
 
 def access_dataframes(spreadsheet_file: Path) -> Dict[str, pandas.DataFrame]:
-    """Access a dataframe from the provided spreadsheet file."""
+    """Access all dataframes from the provided spreadsheet file."""
     name_to_dataframe_dict = pandas.read_excel(spreadsheet_file, sheet_name=None)
     return name_to_dataframe_dict
