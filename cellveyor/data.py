403ddd291c958a4d8fd9e5c3e0beaa354b5fa3e7
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-10-10 11:00:45 -0400

    coms: Add comments to the data.py file.

--- cellveyor/data.py
+++ cellveyor/data.py
@@ -16,8 +16,14 @@ def key_attribute_column_filter(
     sheet_dataframe: pandas.DataFrame, key_attribute_name: str, column_regexp: str
 ) -> Tuple[pandas.DataFrame, pandas.DataFrame]:
     """Extract a region of a dataframe defined by a key attribute and columns that match a regular expression."""
+    # use the provided regular expression to extract from the data frame
+    # only those columns that have a name that matches the regular expression
     selected_columns = sheet_dataframe.filter(regex=column_regexp).dropna()
+    # extract the attribute that has the key name and also select all of
+    # those columns that matched the regular expression
     result_df = sheet_dataframe[
         [key_attribute_name] + list(selected_columns.columns)  # noqa: RUF005
     ].dropna()
+    # for both of the two previous steps, make sure to drop any rows that contain NA values
+    # return the columns that were selected and then the resulting dataframe
     return (selected_columns, result_df)  # type: ignore
