112e1ddf39a74b037350908efa822decbfd1d77c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-10-10 16:25:12 -0400

    coms: Add explanatory comments to the transfer.py file.

--- cellveyor/transfer.py
+++ cellveyor/transfer.py
@@ -14,13 +14,18 @@ DASH = "-"
 def create_fully_qualified_github_repository(
     github_organization: str, github_repository_prefix: str, github_username: str
 ) -> str:
-    """Create a fully qualified GitHub repository name using provied parts."""
+    """Create a fully qualified GitHub repository name using provided naming parts."""
+    # create fully qualified name of a GitHub repository:
+    # --> Add the organization and the prefix of the repository
     fully_qualified_github_repo_name = (
         f"{github_organization}{FORWARD_SLASH}{github_repository_prefix}"
     )
+    # --> Add the dash separator and then the GitHub username
     fully_qualified_github_repo_name = (
         f"{fully_qualified_github_repo_name}{DASH}{github_username}"
     )
+    # Example of a fully qualified name, using the GitHub Classroom format:
+    # Allegheny-Computer-Science-203-F2023/computer-science-203-fall-2023-executable-exam-1-gkapfham
     return fully_qualified_github_repo_name
 
 
@@ -31,12 +36,17 @@ def transfer_reports_to_github(
     github_reports_dict: Dict[str, str],
 ) -> None:
     """Transfer a report to GitHub."""
+    # iterate through the list of completed reports
     for github_report_key in github_reports_dict.keys():
+        # the current key is the name of the GitHub user
         github_username = github_report_key
+        # extract the contents of the report to upload to the pull request comment
         current_github_report_contents = github_reports_dict[github_username]
+        # create the fully qualified name of the GitHub repository
         current_github_repository = create_fully_qualified_github_repository(
             github_organization, github_repository_prefix, github_username
         )
+        # transfer this specific report to the pull request in the GitHub repository
         transfer_report_to_github(
             github_token, current_github_repository, current_github_report_contents
         )
@@ -48,6 +58,15 @@ def transfer_report_to_github(github_token: str, repository: str, report: str) -
     # the use of the provided personal access token
     authorization = Auth.Token(github_token)
     github = Github(auth=authorization)
+    # use the fully qualified name of the GitHub repository to
+    # create a connection to it
     github_repository = github.get_repo(repository)
+    # access the default pull request according to the
+    # convention established by GitHub Classroom
     pull_request = github_repository.get_pull(PULL_REQUEST_ID)
+    # create an issue comment in this specific pull request;
+    # note that this is a stand-alone comment for a pull request
+    # and not specifically connected to the review of the pull
+    # request itself; this is the reason why it is actually
+    # using the issue GitHub API to create the comment
     pull_request.create_issue_comment(report)
