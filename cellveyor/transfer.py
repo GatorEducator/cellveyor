9053a329c5b150218da3269d8d7ccc776ba782de
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-10-10 16:00:32 -0400

    refactor: Use the variable PULL_REQUEST_ID in transfer.py file.

--- cellveyor/transfer.py
+++ cellveyor/transfer.py
@@ -2,6 +2,10 @@
 
 from github import Auth, Github
 
+# the fixed identifier for the pull request that will
+# contain the feedback for the specified repository
+PULL_REQUEST_ID = 1
+
 
 def transfer_report_to_github(github_token: str, report: str) -> None:
     """Transfer a report to GitHub."""
@@ -10,5 +14,5 @@ def transfer_report_to_github(github_token: str, report: str) -> None:
     repository = github.get_repo(
         "Allegheny-Computer-Science-203-F2023/computer-science-203-fall-2023-executable-exam-1-gkapfham"
     )
-    pull_request = repository.get_pull(1)
+    pull_request = repository.get_pull(PULL_REQUEST_ID)
     pull_request.create_issue_comment(report)
