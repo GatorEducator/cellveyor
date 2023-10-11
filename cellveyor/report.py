227680c17e85112d1a5577840206441c67f4b937
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-10-11 17:22:20 -0400

    feat: Improve the report.py functions so that they handle missing report details.

--- cellveyor/report.py
+++ cellveyor/report.py
@@ -9,6 +9,23 @@ HEADER = "header"
 FOOTER = "footer"
 SUMMARY_LABEL = "Here are your summary scores:"
 
+
+def add_feedback_if_exists(
+    report: str, feedback_dict: Dict[str, str], feedback_key: str
+) -> str:
+    """Add feedback to the provided report if a specified key exists in the dictionary."""
+    # the final report will start off as the contents
+    # of the report provided as an argument
+    final_report = report
+    # the feedback dictionary contains the requested key and
+    # thus this function should add the key's value to the report
+    if feedback_key in feedback_dict:
+        feedback = feedback_dict[feedback_key]
+        final_report = final_report + f"{feedback}{NEWLINE}"
+    # return the potentially improved feedback report
+    return final_report
+
+
 def create_per_key_report(
     key_attribute: str,
     result_dataframe: DataFrame,
@@ -27,7 +44,8 @@ def create_per_key_report(
         key_attribute_value = str(row[key_attribute])
         # create a main label for the entire markdown-based report
         current_report = f"**Hello @{key_attribute_value}!**{NEWLINE}{NEWLINE}"
-        current_report = current_report + f"{feedback_dict[HEADER]}{NEWLINE}"
+        # current_report = current_report + f"{feedback_dict[HEADER]}{NEWLINE}"
+        current_report = add_feedback_if_exists(current_report, feedback_dict, HEADER)
         current_report = current_report + f"**{SUMMARY_LABEL}**{NEWLINE}{NEWLINE}"
         # iterate through all of the extracted columns and add them to
         # the report in the following fashion:
