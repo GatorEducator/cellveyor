ebde55e07ada3a880f89755a0e0e6e8016337c13
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-10-11 18:51:40 -0400

    feat: Add a complete reporting function in report.py that handles all segments of data --- probably!

--- cellveyor/report.py
+++ cellveyor/report.py
@@ -11,10 +11,11 @@ HEADER = "header"
 FOOTER = "footer"
 
 SUMMARY_LABEL = "Here are your summary scores:"
+FEEDBACK_LABEL = "Here is some additional feedback to consider:"
 
 
 def add_feedback_if_exists(
-    report: str, feedback_dict: Dict[str, str], feedback_key: str
+    report: str, feedback_dict: Dict[str, str], feedback_key: str, make_list: bool = False
 ) -> str:
     """Add feedback to the provided report if a specified key exists in the dictionary."""
     # the final report will start off as the contents
@@ -24,7 +25,10 @@ def add_feedback_if_exists(
     # thus this function should add the key's value to the report
     if feedback_key in feedback_dict:
         feedback = feedback_dict[feedback_key]
-        final_report = final_report + f"{feedback}{NEWLINE}"
+        if not make_list:
+            final_report = final_report + f"{feedback}{NEWLINE}"
+        else:
+            final_report = final_report + f"- {feedback}"
     # return the potentially improved feedback report
     return final_report
 
@@ -32,6 +36,8 @@ def add_feedback_if_exists(
 def create_feedback_list(feedback_comma_list: str) -> List[str]:
     """Create a list of feedback keys from a string with the lists separated by commas."""
     feedback_list = [key.strip() for key in feedback_comma_list.split(COMMA)]
+    if "nan" in feedback_list:
+        feedback_list.remove("nan")
     return feedback_list
 
 
@@ -47,13 +53,10 @@ def create_per_key_report(
     # --> key: the value for the key attribute (normally a GitHub user name)
     # --> value: an entire report, encoded in markdown for display in the terminal
     # or upload to a markdown-aware platform like a GitHub issue or pull request
-    print(selected_columns)
-    print("Just fiunished first print")
     markdown_reports: Dict[str, str] = {}
     # extract the column(s) that provide extra feedback in a comma-separate list
     # selected_feedback_columns = selected_columns.filter(regex=feedback_regexp).dropna()
     selected_feedback_columns = selected_columns.filter(regex=feedback_regexp)
-    print(selected_feedback_columns)
     selected_columns = selected_columns.drop(selected_feedback_columns, axis=1)  # type: ignore
     # create a unique message for each row in the dataframe
     for index, row in result_dataframe.iterrows():
@@ -74,10 +77,19 @@ def create_per_key_report(
                 current_report + f"- **{column_name}**: {column_value}{NEWLINE}"
             )
         # extract the specific row of feedback from the selected feedback columns
-        feedback_comma_list = selected_feedback_columns.iloc[index, 0]
-        print("TADA")
-        print(feedback_comma_list)
-        current_report = current_report + f"{feedback_dict[FOOTER]}{NEWLINE}"
+        feedback_comma_list = str(selected_feedback_columns.iloc[index, 0])
+        feedback_list = create_feedback_list(feedback_comma_list)
+        if feedback_list:
+            current_report = (
+                current_report
+                + f"{NEWLINE}{NEWLINE}**{FEEDBACK_LABEL}**{NEWLINE}{NEWLINE}"
+            )
+            for feedback_key in feedback_list:
+                current_report = add_feedback_if_exists(
+                    current_report, feedback_dict, feedback_key, True
+                )
+                # current_report = current_report + f"- {current_feedback_value}{NEWLINE}{NEWLINE}"
+        current_report = current_report + f"{NEWLINE}{NEWLINE}{feedback_dict[FOOTER]}{NEWLINE}"
         # now that creation of the current_report is finished, store it
         # inside of the dictionary of the markdown_reports and move to the next one
         markdown_reports[key_attribute_value] = current_report
