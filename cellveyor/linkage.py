e8a12345f5ff25a34adf9def19b9167ccbe833f5
Sabrina Rodriguez <rodriguez03@allegheny.edu>
2023-11-10 09:09:39 -0500

    Linkage attempt

--- cellveyor/linkage.py
+++ cellveyor/linkage.py
@@ -2,24 +2,28 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Load your credentials from the JSON file
-credentials = ServiceAccountCredentials.from_json_keyfile_name('your-credentials.json', ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive'])
+credentials = ServiceAccountCredentials.from_json_keyfile_name(['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive'])
 
 # Authenticate with the Google Sheets API
-gc = gspread.authorize(credentials)
+gc = gspread.authorize(Cellveyor-Sample-Gradebook-Shared)
 
 # Open the Google Sheets document by its title or URL
-spreadsheet = gc.open('Your Spreadsheet Name')
+spreadsheet = gc.open('https://docs.google.com/spreadsheets/d/10mMsPZtKWREUIxqOzTQViSiqBsjeDHWqDMJtxOi-L34/edit#gid=0')
 
 # Access a specific worksheet
 worksheet = spreadsheet.get_worksheet(0)
 
-# Now you can work with the worksheet, for example, reading or writing data
-cell_value = worksheet.cell(1, 1).value
-print(f"Value at (1, 1): {cell_value}")
 
-# To write data
-worksheet.update('A2', 'New Value')
 
-# To read data
-cell_value = worksheet.cell(2, 1).value
-print(f"Value at (2, 1): {cell_value}")
+
+
+
+# cell_value = worksheet.cell(1, 1).value
+# print(f"Value at (1, 1): {cell_value}")
+
+
+# worksheet.update('A2', 'New Value')
+
+
+# cell_value = worksheet.cell(2, 1).value
+# print(f"Value at (2, 1): {cell_value}")
