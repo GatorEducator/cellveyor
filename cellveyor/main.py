d6245910041805e5aeabfc3de9601ceae52e6eef
Will Wolff <willwolff@Wills-MacBook-Pro-2.local>
2023-11-28 16:11:25 -0500

    removed test file and accidental gspread import

--- cellveyor/main.py
+++ cellveyor/main.py
@@ -8,7 +8,6 @@ import typer
 from rich.console import Console
 from rich.markdown import Markdown
 from rich.panel import Panel
-import gspread
 
 from cellveyor import data, filesystem, report, transfer, constants
 
